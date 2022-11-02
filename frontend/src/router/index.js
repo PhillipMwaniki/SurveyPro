30cff0555552764807ffa7302381488867f7035e
Phillip Mwaniki <codezeroprivate@gmail.com>
2022-11-02 10:05:39 +0300

    added authentication check to routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,8 +2,22 @@ import { createRouter, createWebHistory } from "vue-router";
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
 import Dashboard from '../views/Dashboard.vue';
+import Surveys from '../views/Surveys.vue';
+import DefaultLayout from '../components/DefaultLayout.vue';
+import store from "../store/index.js";
 
 const routes = [
+  {
+    path: '/',
+    redirect: '/dashboard',
+    name: 'dashboard',
+    component: DefaultLayout,
+    meta: { requiresAuth: true },
+    children: [
+      { path: '/dashboard', name: 'dashboard', component: Dashboard },
+      { path: '/surveys', name: 'surveys', component: Surveys },
+    ]
+  },
   {
     path: '/login',
     name: 'login',
@@ -14,15 +28,20 @@ const routes = [
     name: 'register',
     component: Register
   },
-  {
-    path: '/dashboard',
-    name: 'dashboard',
-    component: Dashboard
-  }
 ];
 const router = createRouter({
   history: createWebHistory(),
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.state.user.token) {
+    next({ name: 'login'});
+  }else if (store.state.user.token && (to.name === 'login' || to.name === 'register')){
+    next({ name: 'dashboard'});
+  }else{
+    next();
+  }
 });
 
 export default router;
