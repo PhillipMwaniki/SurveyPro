3a4ecf50e931b39cd48c97751fd6a0a467b0ae8f
Phillip Mwaniki <codezeroprivate@gmail.com>
2022-11-02 10:05:15 +0300

    added default layout for authenticated pages

--- frontend/src/views/Surveys.vue
+++ frontend/src/views/Surveys.vue
@@ -4,14 +4,14 @@
 <template>
     <header class="bg-white shadow">
         <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+            <h1 class="text-3xl font-bold tracking-tight text-gray-900">Surveys</h1>
         </div>
     </header>
     <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
             <!-- Replace with your content -->
             <div class="px-4 py-6 sm:px-0">
-                Dashboard here
+                My Surveys
             </div>
             <!-- /End replace -->
         </div>
