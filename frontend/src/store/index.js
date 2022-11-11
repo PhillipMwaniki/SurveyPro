6a6c3b6f5e2421c5417512eae2ab6699c861e6f8
Phillip Mwaniki <codezeroprivate@gmail.com>
2022-11-11 18:38:45 +0300

    added axios and set it up

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,23 +1,49 @@
 import { createStore } from "vuex";
+import axiosClient from "../axios.js";
 
 const store = createStore({
   state: {
     user: {
-      data: {
-        name: 'Tom Cook',
-        email: 'tom@example.com',
-        imageUrl:
-          'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      token: 123
+      data: sessionStorage.getItem('USER'),
+      token: sessionStorage.getItem('TOKEN')
     }
   },
   getters: {},
-  actions: {},
+  actions: {
+    register({commit}, user) {
+      return axiosClient.post('/register', user)
+        .then(({data}) => {
+          commit("setUser", data);
+          return data;
+        });
+    },
+    login({commit}, user) {
+      return axiosClient.post('/login', user)
+        .then(({data}) => {
+          commit("setUser", data);
+          return data;
+        });
+    },
+    logout({commit}) {
+      return axiosClient.post('/logout')
+        .then(() => {
+          commit("logout");
+        });
+    },
+  },
   mutations: {
     logout: state => {
       state.user.data = {};
       state.user.token = null;
+      sessionStorage.removeItem('TOKEN');
+      sessionStorage.removeItem('USER');
+    },
+
+    setUser: (state, userData) => {
+      state.user.data = userData.user;
+      state.user.token = userData.token;
+      sessionStorage.setItem('TOKEN', userData.token);
+      sessionStorage.setItem('USER', userData.user);
     }
   },
   modules: {},
