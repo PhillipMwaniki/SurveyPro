f3c239799a27d88aed501a42a5612222d31eb6c2
Phillip Mwaniki <codezeroprivate@gmail.com>
2022-11-11 13:34:27 +0300

    added page component

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,21 +1,10 @@
 <script setup>
-
+import PageComponent from "../components/PageComponent.vue";
 </script>
 <template>
-    <header class="bg-white shadow">
-        <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-        </div>
-    </header>
-    <main>
-        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <!-- Replace with your content -->
-            <div class="px-4 py-6 sm:px-0">
-                Dashboard here
-            </div>
-            <!-- /End replace -->
-        </div>
-    </main>
+    <PageComponent title="Dashboard">
+        Get started
+    </PageComponent>
 </template>
 <style scoped>
 
--- frontend/src/views/Surveys.vue
+++ frontend/src/views/Surveys.vue
@@ -1,21 +1,10 @@
 <script setup>
-
+import PageComponent from "../components/PageComponent.vue";
 </script>
 <template>
-    <header class="bg-white shadow">
-        <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold tracking-tight text-gray-900">Surveys</h1>
-        </div>
-    </header>
-    <main>
-        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <!-- Replace with your content -->
-            <div class="px-4 py-6 sm:px-0">
-                My Surveys
-            </div>
-            <!-- /End replace -->
-        </div>
-    </main>
+    <PageComponent title="Surveys">
+        My Surveys
+    </PageComponent>
 </template>
 <style scoped>
 
