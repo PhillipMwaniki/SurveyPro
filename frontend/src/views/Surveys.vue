e759931bba7853084335c03f22defc6d8c045bfa
Phillip Mwaniki <codezeroprivate@gmail.com>
2022-11-11 19:18:02 +0300

    add button for creating survey

--- frontend/src/views/Surveys.vue
+++ frontend/src/views/Surveys.vue
@@ -1,9 +1,19 @@
 <script setup>
 import PageComponent from "../components/PageComponent.vue";
+import { PlusIcon } from '@heroicons/vue/24/solid'
+
 </script>
 <template>
-    <PageComponent title="Surveys">
-        My Surveys
+    <PageComponent>
+        <template v-slot="header">
+            <div class="flex justify-between items-center">
+                <h1 class="text-3xl font-bold text-gray-900">Surveys</h1>
+                <router-link :to="{ name: 'surveys.create'}"
+                             class="py-2 px-3 text-sm text-white bg-emerald-500 rounded-md hover:bg-emerald-600">
+                    <PlusIcon class="h-4 w-4 text-white inline-block"/> Survey
+                </router-link>
+            </div>
+        </template>
     </PageComponent>
 </template>
 <style scoped>
