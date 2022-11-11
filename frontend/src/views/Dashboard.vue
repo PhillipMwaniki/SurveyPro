32db365a9496a63ecff60f2b9f21f86ab16789aa
Phillip Mwaniki <codezeroprivate@gmail.com>
2022-11-11 19:17:42 +0300

    changed to using slot in header

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -2,8 +2,13 @@
 import PageComponent from "../components/PageComponent.vue";
 </script>
 <template>
-    <PageComponent title="Dashboard">
-        Get started
+    <PageComponent>
+        <template v-slot="header">
+            <div class="flex justify-between items-center">
+                <h1 class="text-3xl font-bold text-gray-900">Dashboard</h1>
+                <div></div>
+            </div>
+        </template>
     </PageComponent>
 </template>
 <style scoped>
