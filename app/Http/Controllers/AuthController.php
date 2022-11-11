38956c8a777d893b3b8224aee1edce3d6e58befa
Phillip Mwaniki <codezeroprivate@gmail.com>
2022-11-11 18:39:30 +0300

    added logout functionality

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => [
+                'required', 'confirmed', Password::min(8)->mixedCase()->numbers()->symbols()
+            ]
+        ]);
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'email' => 'required|email|exists:users,email',
+            'password' => ['required'],
+            'remember' => 'boolean'
+        ]);
+
+        $remember = $data['remember'] ?? false;
+        unset($data['remember']);
+
+        if (!Auth::attempt($data, $remember)) {
+            return response([
+                'error' => 'The provided credentials are not correct'
+            ], 422);
+        }
+
+        $user = Auth::user();
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function logout()
+    {
+        Auth::user()->currentAccessToken()->delete();
+
+        return response()->noContent();
+    }
+}
