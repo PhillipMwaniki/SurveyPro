b8a7b0a04f39ddf2fd7eb48dfa5a1623e10e5acc
Phillip Mwaniki <codezeroprivate@gmail.com>
2022-12-05 16:11:33 +0300

    added save survey action

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -140,6 +140,24 @@ const store = createStore({
   },
   getters: {},
   actions: {
+    saveSurvey({commit}, survey) {
+      let response;
+      if (survey.id) {
+        response = axiosClient.put(`/survey/${survey.id}`, survey)
+          .then((res) => {
+            commit('updateSurvey', res.data);
+            return res;
+          });
+      }else{
+        response = axiosClient.post(`/survey`, survey)
+          .then((res) => {
+            commit('saveSurvey', res.data);
+            return res;
+          });
+      }
+
+      return response;
+    },
     register({commit}, user) {
       return axiosClient.post('/register', user)
         .then(({data}) => {
@@ -162,6 +180,17 @@ const store = createStore({
     },
   },
   mutations: {
+    saveSurvey: (state, survey) => {
+      state.surveys = [...state.surveys, survey];
+    },
+    updateSurvey: (state, survey) => {
+      state.surveys = state.surveys.map((s) => {
+        if (s.id === survey.data.id) {
+          return survey.data;
+        }
+        return s;
+      });
+    },
     logout: state => {
       state.user.data = {};
       state.user.token = null;
